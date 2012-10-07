a80c820901224cba4d8c5a672c95f04676d979bf
Kenton Jacobsen <kenton.jacobsen@gmail.com>
2012-10-07 09:13:59 -0400

    Adding image field for candidates

--- issue_engine/models.py
+++ issue_engine/models.py
@@ -6,6 +6,7 @@ class Topic(models.Model):
 
 class Candidate(models.Model):
     full_name = models.CharField(max_length=50) 
+    image = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     wp_url = models.CharField(max_length=100)
 
