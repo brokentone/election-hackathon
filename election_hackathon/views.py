54a050761b8650df38212dc5482fc7ebd1069fd0
andrewbt <athompson@azavea.com>
2012-10-07 11:07:25 -0400

    testing a view

--- election_hackathon/views.py
+++ election_hackathon/views.py
@@ -1,6 +1,7 @@
 from django.template import Context, loader
 from issue_engine.models import Statement 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
     list_questions = Statement.objects.all()[:5]
@@ -9,3 +10,10 @@ def home(request):
         'list_questions ': list_questions,
     })
     return HttpResponse(t.render(c))
+
+def indextest(request):
+    issues = ['Economy', 'Govt Spending', 'Energy']
+    statements = ['Mitt said', 'Obama said']
+    return render_to_response('templates/index.html',
+        {"issues": issues},
+        {"statements": statements}, context_instance=RequestContext(request))
