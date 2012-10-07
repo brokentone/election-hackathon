cf6782ed0acd779f422d304a52542852805507eb
Kenton Jacobsen <kenton.jacobsen@gmail.com>
2012-10-07 12:20:35 -0400

    Form changes for JS and JS changes for serialization of sort

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function($) {
     $(".statement-list").sortable();
     $(".statement-list").disableSelection();
+    $('#rank_statements').bind('submit', function() {
+        $('.statement-list').each(function() {
+            console.log($(this).sortable("serialize"));
+        });
+    });
 });
